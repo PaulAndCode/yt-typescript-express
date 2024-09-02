ce0c12a5ef837b7ccd6bc67b9789a67c9c10c72d
Paul Liu <oucl5976@gmail.com>
2024-09-02 19:34:48 +0100

    logger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+
+import morganMiddleware from './middlewares/morganMiddleware';
+import Logger from './logger';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(morganMiddleware);
+
+app.get('/logger', (_, res) => {
+  Logger.error('This is an error log');
+  Logger.warn('This is a warn log');
+  Logger.info('This is a info log');
+  Logger.http('This is a http log');
+  Logger.debug('This is a debug log');
+
+  res.send('Hello world');
+});
 
 // Define a simple route
 app.get('/', (req: Request, res: Response) => {
